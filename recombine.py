5d1af5fca72d7d1c89abaeabc287d61d60172b95
sandzan <164919720+sandzan@users.noreply.github.com>
2025-09-29 11:06:55 +0200

    Added combine with repair and some errors

---
+++ recombine.py
@@ -0,0 +1,31 @@
+import random
+
+def recombine(gen, size):
+    """Devide and recombine boards"""
+    gen2 = []
+    r = random.randint(0, size - 1)
+
+    # Combine with next board
+    for i in range(0, len(gen)):
+        # Last combine with first
+        if i < len(gen)-1:
+            next = i+1
+        else:
+            next = 0
+        
+        # Combine and repair boards and add them to gen2
+        gen2.append(repair(gen[i][0:round((size/2))], gen[next][round((size/2)):size], size))
+        gen2.append(repair(gen[next][0:round((size/2))], gen[i][round((size/2)):size], size))
+
+    return gen2
+
+def repair(first, second, size):
+    """Repair reocuring numbers with missing numbers"""
+    missing = list(set(range(0, size)) - set(first + second))
+    repeating = list(set(second) - (set(range(0, size)) - set(first)))
+
+    # Replace
+    for index, value in enumerate(repeating):
+        second[second.index(value)] = missing[index]
+
+    return first + second
