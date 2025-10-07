4c430be5d22f979a44c3f181b4ecd857e988f763
Eddie Olofsgård <eddieolofsgard@icloud.com>
2025-10-07 18:54:27 +0200

    Moved all statistics to its own folder and fixed plotting problems with plot.py

--- plot.py
+++ plot.py
@@ -9,9 +9,10 @@ def n_queen_plot(itererations, board_sizes_n : list, boards_per_generation, chil
     avg_generations_taken_n = []
     probaility_of_success = []
     max_board_size = 0
+    last_idx_used = 0
     
     #Run info_n_queen solver for data gathering
-    for board_size_n in board_sizes_n:
+    for i, board_size_n in enumerate(board_sizes_n):
         print(f"\nStarting iterations for board size {board_size_n}")
         if(main_algorithm == 0):
             _, generations_taken, time_taken, successes = info_base_n_queen_solver(itererations, board_size_n, boards_per_generation, children_per_generation, mutation_rate, ignore_failed_attempts, max_generations, print_to_file, txt_file_name)
@@ -22,23 +23,27 @@ def n_queen_plot(itererations, board_sizes_n : list, boards_per_generation, chil
         probaility_of_success.append(successes / itererations)
         
         max_board_size = board_size_n
+        last_idx_used = i
         if successes == 0:
             break
 
+    #if we used all board sizes and did fail all cases
+    smallest_board_size = 4
+
     #ploting graph
     plt.subplot(311)
-    plt.plot(board_sizes_n[:-1],avg_time_taken_n)
-    plt.axis((4,max_board_size, 0, math.ceil(max(avg_time_taken_n))))
+    plt.plot(board_sizes_n[:last_idx_used+1],avg_time_taken_n)
+    plt.axis((smallest_board_size,max_board_size, 0, math.ceil(max(avg_time_taken_n))))
     plt.ylabel("Avg time (s)")
     
     plt.subplot(312)
-    plt.plot(board_sizes_n[:-1],avg_generations_taken_n)
-    plt.axis((4,max_board_size, 0, math.ceil(max(avg_generations_taken_n))))
+    plt.plot(board_sizes_n[:last_idx_used+1],avg_generations_taken_n)
+    plt.axis((smallest_board_size,max_board_size, 0, math.ceil(max(avg_generations_taken_n))))
     plt.ylabel("Avg generations taken")
 
     plt.subplot(313)
-    plt.plot(board_sizes_n,probaility_of_success)
-    plt.axis((4,max_board_size, 0, 1))
+    plt.plot(board_sizes_n[:last_idx_used+1],probaility_of_success)
+    plt.axis((smallest_board_size,max_board_size, 0, 1))
     plt.xlabel("Size of n")
     plt.ylabel("Success rate")
     
@@ -50,17 +55,17 @@ def n_queen_plot(itererations, board_sizes_n : list, boards_per_generation, chil
 """
 #ploting graph manually, the below is based on the plot_results.txt in the "base_algorithm" folder. This took 72 minutes to run
 plt.subplot(311)
-plt.plot([x for x in range(4,14)],[0.00234375, 0.0025, 0.003125, 0.23, 4.2509375, 20.06171875, 26.19046875, 29.528125, 34.77828125, 38.84765625])
+plt.plot([x for x in range(4,13)],[0.00296875, 0.0034375, 0.0146875, 0.00453125, 0.69875, 8.13171875, 26.0378125, 33.10421875, 33.164375])
 plt.axis((0,14, 0, 40))
 plt.ylabel("Avg time (s)")
 
 plt.subplot(312)
-plt.plot([x for x in range(4,14)],[0, 0, 0, 0, 42.95, 736.11, 3180.57, 3782.71, 3765.97, 3960.0])
+plt.plot([x for x in range(4,13)],[0,0,1.77,0,94.84,1014.24,3103.22,3771.98,3880.0])
 plt.axis((0,14, 0, 4000))
 plt.ylabel("Avg generations taken")
 
 plt.subplot(313)
-plt.plot([x for x in range(4,15)],[1, 1, 1, 1, 1, 93/100, 26/100, 9/100, 6/100, 1/100, 0])
+plt.plot([x for x in range(4,14)],[1, 1, 1, 1, 1, 86/100, 27/100, 7/100, 3/100, 0])
 plt.axis((0,14, 0, 1))
 plt.xlabel("Size of n")
 plt.ylabel("Success rate")
@@ -77,9 +82,9 @@ if __name__ == "__main__":
     board_sizes_n = [x for x in range(4,51)] #[8, 10, 15, 20, 25, 30, 35, 40, 45, 50]
     #board_sizes_n.extend([35,40,45,50])
 
-    boards_per_generation = 765
-    children_per_generation = 438
-    mutation_rate = 92
+    boards_per_generation = 768
+    children_per_generation = 1000
+    mutation_rate = 20
     max_generations = 4000
     stall_limit = 500
     ignore_failed_attempts = False
